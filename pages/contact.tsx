ee3bd753260d50e3f7ed05a4395232e3e20ce093
Richard Miller <richardmmill@gmail.com>
2021-10-14 21:50:11 -0400

    work on contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+interface UserContact {
+    name: string,
+    email: string,
+    message: string
+}
 
 const Contact = () => {
+    const [name, setName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
+    const [submitted, setSubmitted] = useState<boolean>(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        let contaceData: UserContact = {
+            name, 
+            email, 
+            message
+        }
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={(e) => {handleSubmit(e)}}>
                 <div>
                     <label htmlFor='name'>Name</label>
-                    <input type='text' name='name' ></input>
+                    <br/>
+                    <input type='text' name='name' required onChange={e => setName(e.target.value)}></input>
                 </div>
                 <div>
                     <label htmlFor='email'>Email</label>
-                    <input type='text' name='email' ></input>
+                    <br/>
+                    <input type='text' name='email' required onChange={e => setEmail(e.target.value)}></input>
                 </div>
                 <div>
                     <label htmlFor='message'>Message</label>
-                    <textarea rows={4} cols={50} name='message' ></textarea>
+                    <br/>
+                    <textarea rows={4} cols={20} name='message' required onChange={e => setMessage(e.target.value)}></textarea>
+                </div>
+                <div>
+                    <input type='submit'></input>
                 </div>
             </form>
         </>
