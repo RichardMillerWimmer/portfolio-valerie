292ce69cefb420078bf02fcfc55096a5861d531b
Richard Miller <richardmmill@gmail.com>
2021-10-14 21:29:49 -0400

    contact form build out started, styled components issue fixed with babelrc

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact Component</h1>
-        </div>
+        <>
+            <form>
+                <div>
+                    <label htmlFor='name'>Name</label>
+                    <input type='text' name='name' ></input>
+                </div>
+                <div>
+                    <label htmlFor='email'>Email</label>
+                    <input type='text' name='email' ></input>
+                </div>
+                <div>
+                    <label htmlFor='message'>Message</label>
+                    <textarea rows={4} cols={50} name='message' ></textarea>
+                </div>
+            </form>
+        </>
     )
 }
 
