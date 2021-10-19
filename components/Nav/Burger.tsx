916cc5890a2623b98032734676e4addcb1345592
Richard Miller <richardmmill@gmail.com>
2021-10-18 20:21:43 -0400

    work on nav, moved to bottom for mobile view

--- components/Nav/Burger.tsx
+++ components/Nav/Burger.tsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 import NavList from './NavList';
 
 const StyledBurger = styled.div`
+background-color: #3d3d3d;
 width: 2rem;
 height: 2rem;
 position: fixed;
-top: 15px;
-right: 20px;
+bottom: 15px;
+left: 50%;
+transform: translate(-50%, 0);
 z-index: 20;
 display: none;
 @media (max-width: 768px) {
@@ -23,14 +25,14 @@ div {
   transform-origin: 1px;
   transition: all 0.3s linear;
   &:nth-child(1) {
-    transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+    transform: ${({ open }) => open ? 'rotate(45deg) scale(1.05, 1)' : 'rotate(0)'};
   }
   &:nth-child(2) {
     transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
     opacity: ${({ open }) => open ? 0 : 1};
   }
   &:nth-child(3) {
-    transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+    transform: ${({ open }) => open ? 'rotate(-45deg) scale(1.05, 1)' : 'rotate(0)'};
   }
 }
 `
@@ -45,7 +47,7 @@ const Burger = () => {
                 <div />
                 <div />
             </StyledBurger>
-            <NavList open={open}/>
+            <NavList open={open} setOpen={setOpen}/>
         </>
     )
 }
--- components/Nav/NavList.tsx
+++ components/Nav/NavList.tsx
@@ -7,17 +7,18 @@ list-style: none;
 display: flex;
 flex-flow: row nowrap;
 li {
+    text-align: center;
     padding: 18px 10px;
 }
 @media (max-width: 768px) {
     flex-flow: column nowrap;
     background-color: #3d3d3d;
     position: fixed;
-    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    transform: ${({ open }) => open ? 'translateY(0)' : 'translateY(100%)'};
     top: 0;
     right: 0;
     height: 100vh;
-    width: 120px;
+    width: 100%;
     padding-top: 3.5rem;
     transition: transform 0.3s ease-in-out;
     li {
@@ -27,17 +28,19 @@ li {
 `;
 
 interface NavProps {
-    open: boolean
+    open: boolean,
+    setOpen
 }
 
-const NavList: React.FC<NavProps> = ({ open }: NavProps) => {
+const NavList: React.FC<NavProps> = ({ open, setOpen }: NavProps) => {
+
     return (
         <Ul open={open}>
-            <li><Link href='/'>Home</Link></li>
-            <li><Link href='/about'>About</Link></li>
-            <li><Link href='/contact'>Contact</Link></li>
-            <li><Link href='/cv'>CV</Link></li>
-            <li><Link href='/work'>Work</Link></li>
+            <li onClick={() => setOpen(!open)}><Link href='/' >Home</Link></li>
+            <li onClick={() => setOpen(!open)}><Link href='/about'>About</Link></li>
+            <li onClick={() => setOpen(!open)}><Link href='/contact'>Contact</Link></li>
+            <li onClick={() => setOpen(!open)}><Link href='/cv'>CV</Link></li>
+            <li onClick={() => setOpen(!open)}><Link href='/work'>Work</Link></li>
         </Ul>
     )
 }
