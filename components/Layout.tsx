15e8b15edb470d9d572de84bdfaaeea1949c741f
Richard Miller <richardmmill@gmail.com>
2021-11-23 22:13:18 -0500

    add correct layout to footer component, spaced at botton of content/bottom of vewport, media query for h6 positioning

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,15 +4,19 @@ import Footer from './Footer';
 import Meta from './Meta';
 import Nav from './Nav/Nav';
 
+import styles from '../styles/Layout.module.scss';
+
 const Layout: React.FC = (props) => {
-    const layoutMeta = {title: 'Vals Page', keywords: 'Occupational Therapist, OT, Autism', description: ''}
-    
+    const layoutMeta = { title: 'Vals Page', keywords: 'Occupational Therapist, OT, Autism', description: '' }
+
     return (
         <>
-            <Meta {...layoutMeta}/>
-            <div>
+            <Meta {...layoutMeta} />
+            <div className={styles.layoutContainer}>
                 <Nav />
+                <div className={styles.contentWrapper}>
                     {props.children}
+                </div>
                 <Footer />
             </div>
         </>
