628e2e53b25890b06355995856a185922417dcea
Richard Miller <richardmmill@gmail.com>
2021-11-27 15:30:35 -0500

    setup recaptcha on front end in contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,32 +1,43 @@
-import React, { SyntheticEvent, useState } from 'react'
+import React, { SyntheticEvent, useRef, useState } from 'react'
 import axios from 'axios'
 import { NextPage } from 'next'
 
 import * as yup from 'yup'
 import { FormProvider, useForm, SubmitHandler } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
-
 import FormInput from '../components/FormInput'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 type FormSubmit = {
     name: string,
     email: string,
-    message: string
+    message: string,
+    token?: string
 }
 
 const schema: yup.SchemaOf<FormSubmit> = yup.object().shape({
     name: yup.string().max(20).required(),
     email: yup.string().max(30).email().required(),
-    message: yup.string().max(250).required()
+    message: yup.string().max(250).required(),
+    token: yup.string()
 })
 
 const Contact: NextPage = () => {
     const methods = useForm<FormSubmit>({ resolver: yupResolver(schema) })
-
     const [submitted, setSubmitted] = useState<boolean>(false)
+    const reCaptchaRef = useRef<ReCAPTCHA>()
 
-    const formSubmitHandler: SubmitHandler<FormSubmit> = (data: FormSubmit, e: SyntheticEvent) => {
+
+    const formSubmitHandler: SubmitHandler<FormSubmit> = async (data: FormSubmit, e: SyntheticEvent) => {
         e.preventDefault()
+
+        const token = await reCaptchaRef.current.executeAsync()
+        console.log(token)
+        
+        data = { ...data, token: token }
+        console.log(data)
+
+        reCaptchaRef.current.reset()
 
         axios.post('/api/contact/', data)
             .then(() => {
@@ -57,6 +68,7 @@ const Contact: NextPage = () => {
                         <input type='submit'></input>
                     </div>
                 </form>
+                <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} size='invisible' ref={reCaptchaRef} />
             </FormProvider>
         </>
     )
