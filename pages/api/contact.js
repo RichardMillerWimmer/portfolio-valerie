24d01caa916f510576a116ebf1aab8435a20f1ef
Richard Miller <richardmmill@gmail.com>
2021-11-26 19:17:40 -0500

    nodemailer setup

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,19 +1,39 @@
 require('dotenv').config()
 
-const { EMAIL, PASSWORD } = process.env
+const FROM_EMAIL = process.env.FROM_EMAIL
+const PASSWORD = process.env.PASSWORD
+const TO_EMAIL = process.env.TO_EMAIL
 
-export default function (req, res) {
+export default async function (req, res) {
+
     console.log(req.body)
+
     let nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
         port: 465,
         host: "smtp.gmail.com",
         auth: {
-            user: EMAIL,
+            user: FROM_EMAIL,
             pass: PASSWORD,
         },
         secure: true,
     })
-    
+
+    const mailData = {
+        from: FROM_EMAIL,
+        to: TO_EMAIL,
+        subject: `Message From ${req.body.name}`,
+        text: `${req.body.message} - return email: ${req.body.email}`,
+        html: <div>{req.body.message}</div>
+    }
+
+    transporter.sendMail(mailData, function (err, info) {
+        if (err)
+            console.log('Error', err)
+        else
+            console.log('Else', info)
+    })
+
+    res.status(200)
 
 }
