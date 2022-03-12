170a90e610391b528abb0979827ac147bfdc9a2c
Richard Miller <richardmmill@gmail.com>
2022-03-12 15:15:22 -0500

    presentation data change, presentations input in contentful

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,17 +3,15 @@ import Project from '../components/Project'
 import styles from '../styles/Projects.module.scss';
 // import { client } from '../index'
 
-type Author = string
-
 export type ProjectType = {
     id: number,
     title: string,
     body: string,
     date: string,
-    authors: Author[],
+    authors: string,
     location: string,
     link?: string,
-    image: string,
+    image?: string,
     feature: boolean
 }
 
@@ -21,10 +19,9 @@ export type PresentationType = {
     id: number,
     title: string,
     body: string,
-    date: string,
-    authors: Author[],
-    location: string,
-    image: string,
+    given: string[],
+    authors: string,
+    image?: string,
     feature: boolean
 }
 
@@ -34,7 +31,7 @@ const testProjects: ProjectType[] = [
         title: 'test title 1',
         body: 'test body 1',
         date: 'some date 1',
-        authors: ['Val'],
+        authors: 'Val',
         location: 'journal 1',
         link: 'link 1',
         image: 'image 1',
@@ -45,7 +42,7 @@ const testProjects: ProjectType[] = [
         title: 'test title 2',
         body: 'test body 3',
         date: 'some date 2',
-        authors: ['Val'],
+        authors: 'Val',
         location: 'journal 2',
         link: 'link 2',
         image: 'image 2',
@@ -56,7 +53,7 @@ const testProjects: ProjectType[] = [
         title: 'test title 3',
         body: 'test body 3',
         date: 'some date 3',
-        authors: ['Val'],
+        authors: 'Val',
         location: 'journal 3',
         link: 'link 3',
         image: 'image 3',
@@ -69,9 +66,8 @@ const testPresentations: PresentationType[] = [
         id: 0,
         title: 'test title 1',
         body: 'test body 1',
-        date: 'some date 1',
-        authors: ['Val'],
-        location: 'conference 1',
+        given: ['date - conference'],
+        authors: 'Val',
         image: 'image 1',
         feature: false
     },
@@ -79,9 +75,8 @@ const testPresentations: PresentationType[] = [
         id: 1,
         title: 'test title 2',
         body: 'test body 3',
-        date: 'some date 2',
-        authors: ['Val'],
-        location: 'conference 2',
+        given: ['date - conference'],
+        authors: 'Val',
         image: 'image 2',
         feature: false
     },
@@ -89,9 +84,8 @@ const testPresentations: PresentationType[] = [
         id: 2,
         title: 'test title 3',
         body: 'test body 3',
-        date: 'some date 3',
-        authors: ['Val'],
-        location: 'conference 3',
+        given: ['date - conference'],
+        authors: 'Val',
         image: 'image 3',
         feature: false
     }
@@ -104,6 +98,7 @@ const Projects: NextPage = () => {
         <>
             <section className={styles.projectsContainer}>
                 <h2 className='visualHidden'>Projects</h2>
+                <p>For a full list including: publications, presentations, grants, professional development, memberships, and licenses please see my CV</p>
             </section>
             <div>
                 {testProjects.map((project) => (
